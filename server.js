a359569e27aac27d09def4ba09fe7a07db81f244
Mikkel Davis <mikkeld@gmail.com>
2015-06-30 19:39:38 -0600

    test public

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ mongoose.connection.once('open', function(){console.log('mdb listening on 27017'
 var ProductCtrl = require('./productCtrl.js');
 var CartCtrl = require('./cartCtrl.js');
 var OrderCtrl = require('./orderCtrl.js');
-
+var body-parser = require('body-parser').json();
 var app = require('express')();
 app.use('/', require('express').static(__dirname + '/public'));
-app.use('/', require('body-parser').json());
-app.use('/', require('cors')());
+app.use('/', body-parser);
+// app.use('/', require('cors')());
 app.post('/api/product', ProductCtrl.create);
 app.get('/api/product', ProductCtrl.read);
 app.put('/api/product/:id', ProductCtrl.update);
@@ -45,3 +45,6 @@ app.listen(port, function(){console.log('srv listening on', port)});
 // Birchbox
 // Magazine
 // National Geographic
+
+
+//text message, scour email (some sort of auto),
